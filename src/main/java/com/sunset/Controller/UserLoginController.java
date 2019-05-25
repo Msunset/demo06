e1e02bcca58a2474d29041050228d92c26b03019
sunset <1451795113@qq.com>
2019-05-25 14:15:09 +0800

    完成添加页面的整理，下版本进行添加操作实现

--- src/main/java/com/sunset/Controller/UserLoginController.java
+++ src/main/java/com/sunset/Controller/UserLoginController.java
@@ -1,6 +1,8 @@
 package com.sunset.Controller;
 
+import com.sunset.dao.DepartmentDao;
 import com.sunset.dao.EmployeeDao;
+import com.sunset.entities.Department;
 import com.sunset.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,31 +19,45 @@ import java.util.Map;
 @Controller
 @RequestMapping("/user")
 public class UserLoginController {
+
     @Autowired
     EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
+    @RequestMapping("/addUser")
+    public String addUser(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("deps",departments);
+
+        return "addUser";
+    }
+
     @RequestMapping("/main")
-    public String main(){
+    public String main() {
         return "dashboard";
     }
+
+
     @RequestMapping("/list")
-    public String list(Model model){
+    public String list(Model model) {
         Collection<Employee> all = employeeDao.getAll();
-        model.addAttribute("EmpLists",all);
+        model.addAttribute("EmpLists", all);
         return "list";
 
     }
+
     @PostMapping("/login")
     public String login(@RequestParam("username") String username,
-                        @RequestParam("password")String password,
-                        Map<String,Object> map, HttpSession session){
-        if (!StringUtils.isEmpty(username) && username.equals("123") &&password.equals("123")){
+                        @RequestParam("password") String password,
+                        Map<String, Object> map, HttpSession session) {
+        if (!StringUtils.isEmpty(username) && username.equals("123") && password.equals("123")) {
             //登录成功,向session中存登录信息
-            session.setAttribute("LoginUsername",username);
+            session.setAttribute("LoginUsername", username);
             return "redirect:/main.html";
 
-        }else {
-            map.put("Msg","用户名或密码不正确");
-            return "login";
+        } else {
+            map.put("Msg", "用户名或密码不正确");
+            return "redirect:/login";
         }
 
     }
